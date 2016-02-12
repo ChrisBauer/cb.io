a082d03d497bbfae75e3ece9f04573c6379cb1cf
chrisbauer <cab1113@gmail.com>
2016-02-11 22:39:23 -0500

    updated .gitignore

---
+++ client/angular2/system.config.js
@@ -0,0 +1,18 @@
+System.config({
+    transpiler: 'typescript', 
+    typescriptOptions: { emitDecoratorMetadata: true }, 
+    packages: {
+        'dist/js': {defaultExtension: 'js'},
+        'node_modules': {
+            format: 'cjs',
+            defaultExtension: 'js'
+        }
+    },
+    paths: {
+        'angular2/*': 'node_modules/angular2/*',
+        'angular2/platform/*': 'node_modules/angular2/platform/*',
+        'marked': 'node_modules/marked/marked.min',
+        'rxjs/*': 'node_modules/rxjs/*',
+		'shared/*': '../../shared/src/js/*'
+    }
+    });
