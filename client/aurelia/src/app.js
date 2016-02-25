d680fc56d01138a03fc8a4d7fd5219c316bd428e
chrisbauer <cab1113@gmail.com>
2016-02-24 21:07:41 -0500

    updated aurelia architecture to let markdown handle getting its data

--- client/aurelia/src/app.js
+++ client/aurelia/src/app.js
@@ -1,27 +1,11 @@
-import {HttpClient} from 'aurelia-fetch-client';
 import {inject} from 'aurelia-framework';
 
-@inject(HttpClient)
 export class App {
-    constructor(http) {
-        this.http = http.configure(config => {
-            config.withBaseUrl('http://chrisbauer.io:9000');
-        });
+    constructor() {
+		this.aboutUrl = '/about';
+		this.resumeUrl = '/resume';
     }
 
-    activate() {
-        this.http.fetch('/rethink/about')
-            .then(response => response.json())
-            .then(about => {
-                this.aboutMD = about.content;
-            });
-            
-        this.http.fetch('/rethink/resume')
-            .then(response => response.json())
-            .then(resume => {
-                this.resumeMD = resume.content;
-            });
-    }
 	attached () {
 		// cleanup the "fake" header once the real document has loaded
 		var outsideHeader = document.getElementById('outside-header');
--- client/aurelia/src/components/markdown.js
+++ client/aurelia/src/components/markdown.js
@@ -1,14 +1,23 @@
-import {bindable} from 'aurelia-framework';
+import {inject, bindable} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
 import marked from 'marked';
+import CONSTANTS from '../Constants';
 
-@bindable('md')
+@bindable('srcUrl')
+@inject(HttpClient)
 export class Markdown {
     
-    constructor () {
+    constructor (http) {
+		this.http = http.configure(config => {
+			config.withBaseUrl(CONSTANTS.RETHINK_BASE_URL);
+		});
     }
     
     bind() {
-        this.md = marked(this.md);
+		this.http.fetch(this.srcUrl)
+			.then(response => response.json())
+			.then(response => marked.parse(response.content))
+			.then(content => this.md = content);
     }
     
 }
