477e994820e320c98f029352971f241b880091db
chrisbauer <cab1113@gmail.com>
2016-03-26 00:18:57 -0400

    worked out a solution for react

--- client/react/src/js/components/progress-tracker.js
+++ client/react/src/js/components/progress-tracker.js
@@ -13,23 +13,43 @@ function ProgressTracker(ScrollKeeper) {
             });
             return {
                 anchors: [],
-                currentAnchor: null
+                currentAnchor: null,
+                active: false
+            }
+        },
+        activate: function () { 
+            setTimeout( () => {
+                this.setState({active: true});
+            });
+        },
+        deactivate: function () { this.setState({active: false}) },
+        handleClick: function (event) {
+            if (this.state.active) {
+                const anchor = this.state.anchors[event.currentTarget.dataset.rel];
+                document.documentElement.scrollTop = document.body.scrollTop = anchor.position + 1;
+                this.deactivate();
+            }
+            else {
             }
         },
         render: function () {
             const options = this.state.anchors.map( (anchor, i) => {
-                const classes = classNames({
+                const optionClasses = classNames({
                     option: true,
                     selected: anchor === this.state.currentAnchor
                 });
                 return (
-                    <div className={classes} key={i}>
-                        <a href={anchor.location} title={anchor.title}>{anchor.title} </a>
+                    <div className={optionClasses} key={i}>
+                        <a onClick={this.handleClick} data-rel={i} title={anchor.title}>{anchor.title} </a>
                     </div>
                 );
             });
+            const progressTrackerClasses = classNames({
+                'progress-tracker': true,
+                active: !!this.state.active
+            });
             return (
-                <div className="progress-tracker">
+                <div onMouseEnter={this.activate} onClick={this.activate} onMouseLeave={this.deactivate} className={progressTrackerClasses}>
                     {options}
                 </div>
             );
--- client/react/src/js/components/swap-page.js
+++ client/react/src/js/components/swap-page.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export default function SwapPage () {
     return React.createClass({
         displayName: 'SwapPage',
+        getInitialState: function () {
+            return {
+                active: false
+            };
+        },
+        activate: function () { 
+            setTimeout( () => {
+                this.setState({active: true});
+            });
+        },
+        deactivate: function () { this.setState({active: false}) },
+        handleClick: function (event) {
+            if (this.state.active) {
+                window.location.href = event.currentTarget.dataset.rel;
+            }
+        },
         render: function () {
             const options = Object.keys(this.props.options).map(key => {
                 let option = this.props.options[key];
                 return (
                     <div key={key} className="option">
-                        <a href={option.location} title={option.title}>{option.title} </a>
+                        <a onClick={this.handleClick} data-rel={option.location} title={option.title}>{option.title} </a>
                     </div>
                 );
             });
 
+            const classes = classNames({
+                'swap-page': true,
+                active: this.state.active
+            });
+
             return (
-                <div className="swap-page">
+                <div onMouseEnter={this.activate} onClick={this.activate} onMouseLeave={this.deactivate} className={classes}>
                     {options}
                 </div>
             );
